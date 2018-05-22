bd2a25278b76c8fbae17bd9ab1924f67780dc2d4
Maxim Nasonov <maxim@nasonov.me>
2018-05-22 21:20:43 +0700

    Remove stale code

--- plumbum.py
+++ plumbum.py
@@ -297,10 +297,7 @@ def list_elasticache_clusters(region):
     conn = boto.elasticache.connect_to_region(region)
     req = conn.describe_cache_clusters()
     data = req["DescribeCacheClustersResponse"]["DescribeCacheClustersResult"]["CacheClusters"]
-    if filter_by_kwargs:
-        clusters = [x for x in data if x[filter_by_kwargs.keys()[0]] == filter_by_kwargs.values()[0]]
-    else:
-        clusters = [x for x in data]
+    clusters = [x for x in data]
     return clusters
 
 def main():
